8049b16aea38c52d61b0fba9c4a345a70c7def6e
basyaramuhammad <afghanibasyara@gmail.com>
2021-11-17 23:05:26 +0700

    Upload Github 3
    View Acara, Kesekretariatan, dan Beranda

--- resources/views/admin/dashboard/acara.blade.php
+++ resources/views/admin/dashboard/acara.blade.php
@@ -9,179 +9,90 @@
         <div class="section-header">
             <h1>Acara</h1>
         </div>
-        <div class="card-body">
-            <h4 class="card-title">Striped Table</h4>
-            <p class="card-description">
-                Add class <code>.table-striped</code>
-            </p>
-            <div class="table-responsive">
-                <table class="table table-striped">
-                    <thead>
-                        <tr>
-                            <th>
-                                User
-                            </th>
-                            <th>
-                                First name
-                            </th>
-                            <th>
-                                Progress
-                            </th>
-                            <th>
-                                Amount
-                            </th>
-                            <th>
-                                Deadline
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td class="py-1">
-                                <img src="../../images/faces/face1.jpg" alt="image">
-                            </td>
-                            <td>
-                                Herman Beck
-                            </td>
-                            <td>
-                                <div class="progress">
-                                    <div class="progress-bar bg-success" role="progressbar" style="width: 25%"
-                                        aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-                                </div>
-                            </td>
-                            <td>
-                                $ 77.99
-                            </td>
-                            <td>
-                                May 15, 2015
-                            </td>
-                        </tr>
-                        <tr>
-                            <td class="py-1">
-                                <img src="../../images/faces/face2.jpg" alt="image">
-                            </td>
-                            <td>
-                                Messsy Adam
-                            </td>
-                            <td>
-                                <div class="progress">
-                                    <div class="progress-bar bg-danger" role="progressbar" style="width: 75%"
-                                        aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
-                                </div>
-                            </td>
-                            <td>
-                                $245.30
-                            </td>
-                            <td>
-                                July 1, 2015
-                            </td>
-                        </tr>
-                        <tr>
-                            <td class="py-1">
-                                <img src="../../images/faces/face3.jpg" alt="image">
-                            </td>
-                            <td>
-                                John Richards
-                            </td>
-                            <td>
-                                <div class="progress">
-                                    <div class="progress-bar bg-warning" role="progressbar" style="width: 90%"
-                                        aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
-                                </div>
-                            </td>
-                            <td>
-                                $138.00
-                            </td>
-                            <td>
-                                Apr 12, 2015
-                            </td>
-                        </tr>
-                        <tr>
-                            <td class="py-1">
-                                <img src="../../images/faces/face4.jpg" alt="image">
-                            </td>
-                            <td>
-                                Peter Meggik
-                            </td>
-                            <td>
-                                <div class="progress">
-                                    <div class="progress-bar bg-primary" role="progressbar" style="width: 50%"
-                                        aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-                                </div>
-                            </td>
-                            <td>
-                                $ 77.99
-                            </td>
-                            <td>
-                                May 15, 2015
-                            </td>
-                        </tr>
-                        <tr>
-                            <td class="py-1">
-                                <img src="../../images/faces/face5.jpg" alt="image">
-                            </td>
-                            <td>
-                                Edward
-                            </td>
-                            <td>
-                                <div class="progress">
-                                    <div class="progress-bar bg-danger" role="progressbar" style="width: 35%"
-                                        aria-valuenow="35" aria-valuemin="0" aria-valuemax="100"></div>
-                                </div>
-                            </td>
-                            <td>
-                                $ 160.25
-                            </td>
-                            <td>
-                                May 03, 2015
-                            </td>
-                        </tr>
-                        <tr>
-                            <td class="py-1">
-                                <img src="../../images/faces/face6.jpg" alt="image">
-                            </td>
-                            <td>
-                                John Doe
-                            </td>
-                            <td>
-                                <div class="progress">
-                                    <div class="progress-bar bg-info" role="progressbar" style="width: 65%"
-                                        aria-valuenow="65" aria-valuemin="0" aria-valuemax="100"></div>
-                                </div>
-                            </td>
-                            <td>
-                                $ 123.21
-                            </td>
-                            <td>
-                                April 05, 2015
-                            </td>
-                        </tr>
-                        <tr>
-                            <td class="py-1">
-                                <img src="../../images/faces/face7.jpg" alt="image">
-                            </td>
-                            <td>
-                                Henry Tom
-                            </td>
-                            <td>
-                                <div class="progress">
-                                    <div class="progress-bar bg-warning" role="progressbar" style="width: 20%"
-                                        aria-valuenow="20" aria-valuemin="0" aria-valuemax="100"></div>
-                                </div>
-                            </td>
-                            <td>
-                                $ 150.00
-                            </td>
-                            <td>
-                                June 16, 2015
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-
         <div class="section-body">
+            <div class="container-fluid">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="callout callout-info">
+                            <h5>Informasi</h5>
+                            <p>
+                                Unggah Dokumen LPJ Kegiatan yang akan diajukan. Ukuran maksimum dokumen: 25 MB.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">Unggah Dokumen LPJ Kegiatan </h3>
+                    </div>
+                    <form class="form-horizontal" method="post" enctype="multipart/form-data" action="/Sign/Create"
+                        novalidate="novalidate">
+                        <div class="card-body">
+                            <div class="form-group row">
+                                <label class="col-sm-2 col-form-label" for="PasswordSertifikat">Nama Organisasi
+                                    Mahasiswa</label>
+                                <div class="col-sm-10">
+                                    <input type="text" class="form-control" autocomplete="new-password" minlength="4"
+                                        data-val="true" data-val-required="The PasswordSertifikat field is required."
+                                        id="PasswordSertifikat" name="PasswordSertifikat"
+                                        placeholder="Masukan nama orgasisasi">
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-sm-2 col-form-label" for="PasswordSertifikat">Nama Kegiatan
+                                    Organisasi</label>
+                                <div class="col-sm-10">
+                                    <input type="text" class="form-control" autocomplete="new-password" minlength="4"
+                                        data-val="true" data-val-required="The PasswordSertifikat field is required."
+                                        id="PasswordSertifikat" name="PasswordSertifikat"
+                                        placeholder="Unggah dokumen LPJ yang akan diajukan">
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-sm-2 col-form-label" for="PasswordSertifikat">Jenis Kegiatan</label>
+                                <div class="col-sm-10">
+                                    <div class="form-check form-check-inline">
+                                        <input class="form-check-input" type="checkbox" id="inlineCheckbox1"
+                                            value="option1">
+                                        <label class="form-check-label" for="inlineCheckbox1">Dokumen LPJ RKAT</label>
+                                    </div>
+                                    <div class="form-check form-check-inline">
+                                        <input class="form-check-input" type="checkbox" id="inlineCheckbox2"
+                                            value="option2">
+                                        <label class="form-check-label" for="inlineCheckbox2">Dokumen LPJ Non-RKAT</label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-sm-2 col-form-label" for="Dokumen">Dokumen LPJ</label>
+                                <div class="col-sm-10">
+                                    <input type="file" class="form-control-file" accept="application/pdf" data-val="true"
+                                        data-val-required="The Dokumen field is required." id="Dokumen" name="Dokumen">
+                                </div>
+                            </div>
+                            {{-- <div class="form-group row">
+                            <label class="col-sm-2 col-form-label">Nama Dokumen</label>
+                            <div class="col-sm-10">
+                                <span class="text-muted" id="JudulDokumen">(otomatis berdasarkan nama file)</span>
+                                <input type="hidden" data-val="true"
+                                    data-val-required="The NomorHalamanTtd field is required." id="NomorHalamanTtd"
+                                    name="NomorHalamanTtd" value="1">
+                            </div>
+                        </div> --}}
+                        </div>
+                        <!-- /.card-body -->
+                        <div class="card-footer">
+                            <button type="submit" class="btn btn-success">
+                                <i class="fas fa-angle-double-right"></i>
+                                Unggah LPJ
+                            </button>
+                        </div>
+                        <!-- /.card-footer -->
+                        {{-- <input name="__RequestVerificationToken" type="hidden"
+                        value="CfDJ8AwUZWNvh05ItjDzOB9O2kGg1DbGFRPYEaK47gH8USQ2CYGH2CNgyoum0e6SHfMt01noF6dER4ROOtq-r6PDWtQT7DYJ9GVqL8lLGe828BXFYWZh2U9_ojvtiaChnsOdyi1VVG2ncv860cnvmhfqw1-JIoMLqsqDaty2NncnCErt8hPSD52nzX7I2L7w246ypQ"> --}}
+                    </form>
+                </div>
+            </div>
         </div>
     </section>
 @endsection
--- resources/views/admin/dashboard/kesekretariatan.blade.php
+++ resources/views/admin/dashboard/kesekretariatan.blade.php
@@ -9,7 +9,94 @@
         <div class="section-header">
             <h1>Kesekretariatan</h1>
         </div>
-
+        <div class="container-fluid">
+            <div class="card">
+                <div class="card-header">
+                    <div class="callout callout-info">
+                        <h5>Informasi</h5>
+                        <p>
+                            Unggah Dokumen Proposal Kegiatan yang akan diajukan. Ukuran maksimum dokumen: 25 MB.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Unggah Dokumen Proposal Kegiatan </h3>
+                </div>
+                <form class="form-horizontal" method="post" enctype="multipart/form-data" action="/Sign/Create"
+                    novalidate="novalidate">
+                    <div class="card-body">
+                        <div class="form-group row">
+                            <label class="col-sm-2 col-form-label" for="PasswordSertifikat">Nama Organisasi
+                                Mahasiswa</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" autocomplete="new-password" minlength="4"
+                                    data-val="true" data-val-required="The PasswordSertifikat field is required."
+                                    id="PasswordSertifikat" name="PasswordSertifikat" placeholder="Masukan nama orgasisasi">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-sm-2 col-form-label" for="PasswordSertifikat">Nama Kegiatan Organisasi</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" autocomplete="new-password" minlength="4"
+                                    data-val="true" data-val-required="The PasswordSertifikat field is required."
+                                    id="PasswordSertifikat" name="PasswordSertifikat"
+                                    placeholder="Unggah dokumen proposal yang akan diajukan">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-sm-2 col-form-label" for="PasswordSertifikat">Jenis Kegiatan</label>
+                            <div class="col-sm-10">
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+                                    <label class="form-check-label" for="inlineCheckbox1">Proposal RKAT</label>
+                                </div>
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
+                                    <label class="form-check-label" for="inlineCheckbox2">Proposal Non-RKAT</label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-sm-2 col-form-label" for="PasswordSertifikat">Tanggal Kegiatan</label>
+                            <div class="col-sm-2">
+                                <input type="date" class="form-control" autocomplete="new-password" minlength="4"
+                                    data-val="true" data-val-required="The PasswordSertifikat field is required."
+                                    id="PasswordSertifikat" name="PasswordSertifikat"
+                                    placeholder="Unggah dokumen proposal yang akan diajukan">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-sm-2 col-form-label" for="Dokumen">Proposal</label>
+                            <div class="col-sm-10">
+                                <input type="file" class="form-control-file" accept="application/pdf" data-val="true"
+                                    data-val-required="The Dokumen field is required." id="Dokumen" name="Dokumen">
+                            </div>
+                        </div>
+                        {{-- <div class="form-group row">
+                            <label class="col-sm-2 col-form-label">Nama Dokumen</label>
+                            <div class="col-sm-10">
+                                <span class="text-muted" id="JudulDokumen">(otomatis berdasarkan nama file)</span>
+                                <input type="hidden" data-val="true"
+                                    data-val-required="The NomorHalamanTtd field is required." id="NomorHalamanTtd"
+                                    name="NomorHalamanTtd" value="1">
+                            </div>
+                        </div> --}}
+                    </div>
+                    <!-- /.card-body -->
+                    <div class="card-footer">
+                        <button type="submit" class="btn btn-success">
+                            <i class="fas fa-angle-double-right"></i>
+                            Unggah Proposal
+                        </button>
+                    </div>
+                    <!-- /.card-footer -->
+                    {{-- <input name="__RequestVerificationToken" type="hidden"
+                        value="CfDJ8AwUZWNvh05ItjDzOB9O2kGg1DbGFRPYEaK47gH8USQ2CYGH2CNgyoum0e6SHfMt01noF6dER4ROOtq-r6PDWtQT7DYJ9GVqL8lLGe828BXFYWZh2U9_ojvtiaChnsOdyi1VVG2ncv860cnvmhfqw1-JIoMLqsqDaty2NncnCErt8hPSD52nzX7I2L7w246ypQ"> --}}
+                </form>
+            </div>
+        </div>
         <div class="section-body">
         </div>
     </section>
