54912dfc126e083252babfc456b3b5ce98b88275
zakariazikri <zakariazikri@apps.ipb.ac.id>
2021-11-19 10:46:30 +0700

    Konsultasi Changes
    Chat

--- resources/views/admin/dashboard/konsultasi.blade.php
+++ resources/views/admin/dashboard/konsultasi.blade.php
@@ -11,6 +11,21 @@
         </div>
 
         <div class="section-body">
+            <div class="card chat-box" id="mychatbox">
+                <div class="card-header">
+                    <h4>Pembimbing</h4>
+                </div>
+                <div class="card-body chat-content">
+                </div>
+                <div class="card-footer chat-form">
+                    <form>
+                        <input type="text" class="form-control" placeholder="Type a message">
+                        <button class="btn btn-primary">
+                            <i class="far fa-paper-plane"></i>
+                        </button>
+                    </form>
+                </div>
+            </div>
         </div>
     </section>
 @endsection
