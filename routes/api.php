49b9d99f66b27dc36b5d8b5e4f75ac5e9018d8f3
Prakasita <92855358+Prakasita@users.noreply.github.com>
2021-11-30 21:56:46 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,9 @@
 use Illuminate\Http\Request;
 
 /*
-|--------------------------------------------------------------------------
+|---------------------------------------------------------------------------
 | API Routes
-|--------------------------------------------------------------------------
+|---------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
