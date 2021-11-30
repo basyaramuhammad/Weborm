412333f12135d63e7c8241a7142f4ca35fd14d2e
Rinaldi Ariyanda <92636043+rinaldiar@users.noreply.github.com>
2021-11-30 21:51:11 +0700

    update migrations_users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
+
 class CreateUsersTable extends Migration
 {
     /**
@@ -24,6 +25,7 @@ class CreateUsersTable extends Migration
         });
     }
 
+    
     /**
      * Reverse the migrations.
      *
